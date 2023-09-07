a2c91741aafc997fd2c9a8a0da03e6a52a1af151
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-07 06:44:37 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { Switch, Route, Link } from 'react-router-dom'
+import Acerca from './Acerca.jsx'
+import Contacto from './Contacto.jsx'
+import Inicio from './Inicio.jsx'
+
+
+
+
 function App() {
-  return <div className="App">hola mundo</div>
+  return 
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/acerca">Acerca</Link>
+        </li>
+        <li>
+          <Link to="/contacto">Contacto</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/acerca">
+        <Acerca />
+      </Route>
+      <Route path="/contacto">
+        <Contacto />
+      </Route>
+      <Route path="/">
+        <Inicio />
+      </Route>
+    </Switch>
+  </div>
 }
 
 export default App
